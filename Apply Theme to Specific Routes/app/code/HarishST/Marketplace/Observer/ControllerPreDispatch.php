9cf9fa76e596939e6600e57e4ff73ff347e1d85c
HarishST <harishstprojects@gmail.com>
2021-12-03 08:18:14 +0530

    Event changed. Docblocks added/updated.

--- Apply Theme to Specific Routes/app/code/HarishST/Marketplace/Observer/ControllerPreDispatch.php
+++ Apply Theme to Specific Routes/app/code/HarishST/Marketplace/Observer/ControllerPreDispatch.php
@@ -11,9 +11,21 @@ use Magento\Framework\View\DesignInterface;
 class ControllerPreDispatch implements ObserverInterface
 {
 
+    /**
+     * @var DesignInterface
+     */
     private DesignInterface $design;
+
+    /**
+     * @var UrlInterface
+     */
     private UrlInterface $url;
 
+    /**
+     * ControllerPreDispatch constructor.
+     * @param DesignInterface $design
+     * @param UrlInterface $url
+     */
     public function __construct(
         DesignInterface $design,
         UrlInterface $url
@@ -29,8 +41,8 @@ class ControllerPreDispatch implements ObserverInterface
     public function execute(Observer $observer): void
     {
         // Apply default Luma theme, if the route is marketplace or mass upload.
-        $exploded = explode('/', parse_url($this->url->getCurrentUrl(), PHP_URL_PATH));
-        if ( $exploded[1] == 'marketplace' || $exploded[1] == 'mpmassupload' ) {
+        $urlSegments = explode('/', parse_url($this->url->getCurrentUrl(), PHP_URL_PATH));
+        if ( $urlSegments[1] == 'marketplace' || $urlSegments[1] == 'mpmassupload' ) {
             $this->design->setDesignTheme('Magento/luma', 'frontend');
         }
 
